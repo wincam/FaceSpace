71a9c501df76527d3fec743ce6d7fbbaa4f47527
ethan <ejacks42@uwo.ca>
2017-02-06 16:40:52 -0500

    Adding StatusPostController to repo.

---
+++ grails-app/controllers/facespace/StatusPostController.groovy
@@ -0,0 +1,36 @@
+package facespace
+
+import grails.rest.RestfulController
+
+class StatusPostController extends RestfulController{
+
+    static allowedMethods = [postStatus: 'POST']
+
+    StatusPostController(){
+        super(StatusPost);
+    }
+
+    def index() {
+        // some default behaviour could be here
+    }
+
+    def fail() {
+        render "Fail."
+    }
+
+    // Our first POST-enabled method
+    // We just want to get the status text and save it to the profile.
+    def postStatus(){
+        def uname = params.userName
+        def account = UserAccount.find{userName == uname}
+        if(account != null){
+            def profile = account.getProfile()
+            def statusText = params.statusText
+            new StatusPost(ownerProfile:profile, statusText:statusText).save()
+            response.status = 200
+        }
+        else{
+            response.status = 404
+        }
+    }
+}
